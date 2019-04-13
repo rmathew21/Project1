03d1b10695cce9273a9826ef26df38545c7a4536
rob <robmathew21@gmail.com>
2019-04-13 01:49:28 -0500

    ajax call

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,36 @@
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=euu1LQJ75njV7gh2yYrVmDf5jG8ldshP&q=programmer&limit=2&offset=0&rating=PG&lang=en";
 
+   $.ajax({
+     url: queryURL,
+     method: "GET"
+   })
+   
+    .then(function(response) {
+    console.log(queryURL);
 
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '{2162737323781924}',
-      cookie     : true,
-      xfbml      : true,
-      version    : '{v3.2}'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
+    console.log(response);
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+    // storing data from the AJAX request in the results variable
+    var results = response.data;
+
+    // looping through each result item
+    for (var i = 0; i < results.length; i++) {
+
+    // creating and storing a div tag
+    var programmer = $("<div>");
+
+    var p = $("<p>").text("Rating: " + results[i].rating);
+
+    var programmerImage = $("<img>").attr('src', results[i].images.fixed_height.url);
+
+    programmer.append(p);
+    programmer.append(programmerImage);
+
+    $("#gifs-appear-here").prepend(programmerImage);
+
+    }
+  });
+
+  
+
+   
