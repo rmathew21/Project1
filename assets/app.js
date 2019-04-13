4d7e833ad540e0281c8026bd99240ab6d360c877
artpolly1 <art.polly1@gmail.com>
2019-04-13 13:45:28 -0500

    style changes

--- assets/app.js
+++ assets/app.js
@@ -23,55 +23,6 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=euu1LQJ75njV7gh2yYr
     .then(function(response) {
     console.log(queryURL);
 
-  // window.fbAsyncInit = function() {
-  //   FB.init({
-  //     appId      : '{2162737323781924}',
-  //     cookie     : true,
-  //     xfbml      : true,
-  //     version    : '{v3.2}'
-  //   });
-      
-  //   FB.AppEvents.logPageView();   
-      
-  // };
-
-  // (function(d, s, id){
-  //    var js, fjs = d.getElementsByTagName(s)[0];
-  //    if (d.getElementById(id)) {return;}
-  //    js = d.createElement(s); js.id = id;
-  //    js.src = "https://connect.facebook.net/en_US/sdk.js";
-  //    fjs.parentNode.insertBefore(js, fjs);
-  //  }(document, 'script', 'facebook-jssdk'));
-
-  //Click event for the submit button. When user clicks Submit button to sign in or create profile...
-
-  $("#submitBtn").on("click", function(event) {
-          
-    // Prevent form from submitting
-        event.preventDefault();
-        
-    //Grab the values that the user enters in the text boxes in the "Add train" section. Store the values in variables.
-      let uname = $("#inputUserName").val().trim();
-    let psWord = $("#inputPassWord").val().trim();
-    
-    console.log(uname);
-    console.log(psWord);
-  
-    if(uname === "" || psWord === "") {
-    $('#invalidUserName').empty();
-    $('#invalidPsword').empty();
-    }
-    else if (uname === null || psWord === null) {
-    $('#invalidUserName').empty();
-    $('#invalidPsword').empty();
-  
-    }
-    else if (uname === "project1" && psWord === "12345") {
-      window.open("../project1/home-nav.html");
-  
-    }
-  
-          });
     console.log(response);
 
     // storing data from the AJAX request in the results variable
@@ -93,6 +44,37 @@ var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=euu1LQJ75njV7gh2yYr
     $("#gifs-appear-here").prepend(programmerImage);
 
     }
+  
+          });
+
+    
+
+
+    $("#submitBtn").on("click", function(event) {
+          
+      // Prevent form from submitting
+          event.preventDefault();
+          
+      //Grab the values that the user enters in the text boxes in the "Add train" section. Store the values in variables.
+        let uname = $("#inputUserName").val().trim();
+      let psWord = $("#inputPassWord").val().trim();
+      
+      console.log(uname);
+      console.log(psWord);
+    
+      if(uname === "" || psWord === "") {
+      $('#invalidUserName').empty();
+      $('#invalidPsword').empty();
+      }
+      else if (uname === null || psWord === null) {
+      $('#invalidUserName').empty();
+      $('#invalidPsword').empty();
+    
+      }
+      else if (uname === "project1" && psWord === "12345") {
+        window.open("../project1/home-nav.html");
+    
+      }
   });
 
   
